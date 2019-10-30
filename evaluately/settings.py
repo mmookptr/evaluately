e79dec3c9bcda16fb7e810b2955fa75bc4400b54
sidtipat.k <s.kietchai@outlook.com>
2019-10-30 13:21:31 +0700

    fix registration

--- evaluately/settings.py
+++ evaluately/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main.apps.MainConfig',
-    'register.apps.RegisterConfig'
+    'register',
 ]
 
 MIDDLEWARE = [
