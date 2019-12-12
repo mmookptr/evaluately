73759ab798b16e96207110d1a3ac75fe6eac5d9e
Patteera Likitamnuayporn <mmookptr@Patteeras-MacBook-Pro.local>
2019-12-12 17:31:26 +0700

    Move middleware

--- evaluately/settings.py
+++ evaluately/settings.py
@@ -54,11 +54,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'social_django.middleware.SocialAuthExceptionMiddleware',
-]
-
-MIDDLEWARE_CLASSES = (
     'whitenoise.middleware.WhiteNoiseMiddleware',
-)
+]
 
 ROOT_URLCONF = 'evaluately.urls'
 
