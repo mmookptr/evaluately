589e7e55cd014516d2c0cc1df3c9428da56e4d50
Patteera Likitamnuayporn <mmookptr@Patteeras-MacBook-Pro.local>
2019-12-12 20:39:27 +0700

    Edit app to use postgres on production

--- evaluately/settings.py
+++ evaluately/settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 """
 Django settings for evaluately project.
 
@@ -84,10 +85,7 @@ WSGI_APPLICATION = 'evaluately.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, config('DATABASES')),
-    }
+    'default': dj_database_url.parse(config('DATABASE_URL'), conn_max_age=600)
 }
 
 
