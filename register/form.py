2b0da55ea816f1d1e8218647863da198b6b45a7e
Patteera L <mmookptr@hotmail.com>
2019-12-13 21:47:46 +0700

    Add admin code for register as admin

--- register/form.py
+++ register/form.py
@@ -1,3 +1,15 @@
 from django import forms
-from .models import User
+from .models import AppUser
+from django.contrib.auth.forms import UserCreationForm
 
+class AppUserCreationForm(UserCreationForm):
+    class Meta:
+        model = AppUser
+        fields = ('username', 'password1', 'password2', 'is_admin')
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_admin = self.cleaned_data['is_admin'] == 'iamadmin'
+        if commit:
+            user.save()
+        return user
--- register/views.py
+++ register/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from .form import AppUserCreationForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import User
 
 
 def register(request):
-    form = UserCreationForm()
+    form = AppUserCreationForm()
     return render(request=request,
                   template_name="register/register.html",
                   context={"form": form})
@@ -15,7 +16,7 @@ def register(request):
 
 def get_name(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = AppUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
 
